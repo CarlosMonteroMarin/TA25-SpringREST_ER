cc7282408684c836940ba14bbcd6ba1aabe9c7c8
Carlos Montero Marín <46814503+CarlosMonteroMarin@users.noreply.github.com>
2022-11-15 09:29:07 +0100

    Add files via upload

---
+++ TA25_Spring_REST_ER_EJ4/src/main/java/com/TA25_EJ4/Ta25SpringRestErEj4Application.java
@@ -0,0 +1,13 @@
+package com.TA25_EJ4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ta25SpringRestErEj4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ta25SpringRestErEj4Application.class, args);
+	}
+
+}
---
+++ TA25_Spring_REST_ER_EJ4/src/test/java/com/TA25_EJ4/Ta25SpringRestErEj4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TA25_EJ4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ta25SpringRestErEj4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
